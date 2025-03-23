c20de9da311ea7c9a8b38f99677ab2e0134c4233
Daniel Rauch <rauch.daniel1@gmx.net>
2025-03-23 09:31:19 +0100

    initialize add two numbers

---
+++ add_two_numbers/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/smokyDee/leetcode
+
+go 1.22.3
